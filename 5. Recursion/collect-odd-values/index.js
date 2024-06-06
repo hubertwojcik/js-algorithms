c460672c86c526bce05ba1dbd4069997ca279b6a
Hubert Wójcik <hwojcikk@gmail.com>
2024-06-06 07:55:56 +0200

    collec todd values

--- 5. Recursion/collect-odd-values/index.js
+++ 5. Recursion/collect-odd-values/index.js
@@ -1,16 +1,15 @@
 function collectOddValues(arr) {
-  let result = [];
+  let newArr = [];
 
-  function helper(helperInput) {
-    if (helperInput.length === 0) return;
-
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
-    }
-    helper(helperInput.slice(1));
+  if (arr.length === 0) {
+    return newArr;
   }
 
-  helper(arr);
+  if (arr[0] % 2 !== 0) {
+    return newArr.push(arr[0]);
+  }
 
-  return result;
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+
+  return newArr;
 }
